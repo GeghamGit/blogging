d74a527116106af2b612a7f15ab677c86977e49f
GeghamGit <gegham.wdallc@gmail.com>
2020-06-01 18:11:47 +0400

    Update blogging project

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const express = require("express");
 const path = require("path");
 const logger = require("morgan");
 const helmet = require('helmet');
-// const swaggerUi = require('swagger-ui-express');
-// const swaggerJsdoc = require('swagger-jsdoc');
-// const conf = require('./config');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const conf = require('./config');
 
 const blogRouter = require("./routes/blogs");
 const userRouter = require("./routes/users");
 
 const app = express();
-// const specs = swaggerJsdoc(conf.swagger);
+const specs = swaggerJsdoc(conf.swagger);
 
 app.use(helmet());
 app.use(logger("dev"));
@@ -18,7 +18,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 app.use("/blogs", blogRouter);
 app.use("/auth", userRouter);
 
